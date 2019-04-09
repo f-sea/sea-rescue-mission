a77b3a1c9c30110d8ae2f82942f0ff6919026467
Jakendarth <kougiatsosn@gmail.com>
2019-04-09 20:15:44 +0300

    Base plot added

--- Code/limenes.py
+++ Code/limenes.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 #----------------------------------------Define required classes------------------------------------------------------
 #---------------------------------------------------------------------------------------------------------------------
 
@@ -36,10 +37,10 @@ class people_in_danger:  # Info by special tranceiver embedded in life jacket
 
 
 # Rescue fleet available by port
-ship1 = ships('Matrix 450', 6 * 0.5144, 6, 3, 0, np.random.choice(5, 1))  # Matrix 450 by Norsafe
+ship1 = ships('Matrix 450', 6 * 0.5144, 6, 3, 0, np.random.choice(7, 1))  # Matrix 450 by Norsafe
 ship1.no_total
-ship2 = ships('Maya 850', 25 * 0.5144, 17, 5, 0, np.random.choice(5, 1))  # Maya 850 by Norsafe
-ship3 = ships('FRC 1204', 35 * 0.5144, 56, 10, 0, np.random.choice(5, 1))  # frc 1204 by fastrescuecraft.nl
+ship2 = ships('Maya 850', 25 * 0.5144, 17, 5, 0, np.random.choice(7, 1))  # Maya 850 by Norsafe
+ship3 = ships('FRC 1204', 35 * 0.5144, 56, 10, 0, np.random.choice(7, 1))  # frc 1204 by fastrescuecraft.nl
 # ship1.are_available()
 print("Case study's total ship fleet \n")
 for ship in [ship1, ship2, ship3]:
@@ -51,16 +52,34 @@ ship1.time_to_save(4)
 
 '''People distribution on sea . The accident happened 2 nautical miles out of Paros's port. There were a total of 533 passengers (fleet and normal)
 81 people lost their lives   Source: https://www.news247.gr/afieromata/expres-samina-17-chronia-meta-to-nayagio-stin-paro-me-toys-81-nekroys.6519323.html'''
-centerX = 1
-centerY = 1
-R_max_possible = 8
+port_x=float(input("Provide port's x coordinate \n"))
+port_y=float(input("Provide port's y coordinate \n"))
+centerX = float(input("Provide wreck's x coordinate \n"))
+centerY = float(input("Provide wreck's y coordinate \n"))
+R_max_possible = 500 # Distance in which there might lay passengers after the wreck [m]
 people_on_board = 533
+people_dead=33
 x_pos = np.array([])
 y_pos = np.array([])
+x_circle=np.array([])
+y_circle=np.array([])
 for person in range(1, people_on_board):
-    r = R_max_possible * np.sqrt(np.random.rand())
-    theta = np.random.rand() * 2 * np.pi
+    r = R_max_possible * np.sqrt(np.random.uniform(0,1))
+    theta = np.random.uniform() * 2 * np.pi
     x = centerX + r * np.cos(theta)
     y = centerY + r * np.sin(theta)
-    np.append(x_pos, x)
-    np.append(y_pos, y)
+    x_circle=np.append(x_circle,[centerX + R_max_possible * np.cos(theta)])
+    y_circle=np.append(y_circle,[centerY + R_max_possible * np.sin(theta)])
+    x_pos=np.append(x_pos,[x])
+    y_pos=np.append(y_pos,[y])
+
+fig=plt.figure()
+plt.plot(port_x,port_y,'gx')
+plt.plot(x_pos,y_pos,'ro')
+plt.plot(centerX,centerY,'bx')
+plt.plot(x_circle,y_circle,'bo')
+plt.xlabel('x [m]')
+plt.ylabel('y [m]')
+plt.grid()
+plt.title('Problem placement')
+plt.show()
